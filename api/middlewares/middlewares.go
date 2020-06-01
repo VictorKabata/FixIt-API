e0f0dafecc35816166b54359abc0ec284d0651fe
Victor Kabata Ndung'u <39780120+VictorKabata@users.noreply.github.com>
2020-06-01 21:01:17 +0300

    Finished on the middlewares.go file

--- api/middlewares/middlewares.go
+++ api/middlewares/middlewares.go
@@ -1,11 +1,29 @@
 package middlewares
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
+
+	"github.com/victorkabata/FixIt/api/auth"
+	"github.com/victorkabata/FixIt/api/responses"
+)
 
 //Formats all responses to JSON format
 func SetMiddlewareJSON(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
+		next(w, r)
+	}
+}
+
+//Checks the validity of the tokens provided.
+func SetMiddlewareAuthentication(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := auth.TokenValid(r)
+		if err != nil {
+			responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
+			return
+		}
 		next(w, r)
 	}
 }
